51aeec1005852d145bcaca445d223ed125205675
Hoko Le <hokole527@gmail.com>
2019-10-03 16:14:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,8 +3,8 @@ def map(source_array)
   i = 0 
   negative_array = []
   while i < source_array.count do
-    yield(source_array[i])
-    source_array << 
+    yield(source_array[i]) negative_array
+
 end
 negative_array
 end
